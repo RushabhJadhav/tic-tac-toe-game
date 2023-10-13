b62492ec198bdf0b02ece4746ab3fd4aa978c074
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2023-10-13 11:28:02 +0530

    added player turns

--- modules/index.js
+++ modules/index.js
@@ -3,26 +3,42 @@ const playerTwo = [];
 
 const box = document.getElementsByClassName('box');
 let boxClicked = false;
+let turn = false;
 
 // console.log(box)
 
 for(let i = 0; i < box.length; i++) {
     box[i].addEventListener('click', () => {
         boxClicked = !boxClicked;
-        box[i].innerHTML = boxClicked ? "X" : "0";
-        playerOne.push(i)
+        turn = !turn;
+        if(turn) {
+            box[i].innerHTML = "" && boxClicked ? "X" : "0";
+            playerOne.push(i)
+        } else {
+            box[i].innerHTML = boxClicked ? "0" : "X";
+            playerTwo.push(i)
+        }
+        checkWinner()
         console.log(playerOne)
     })
 }
 
 const winningCombinations = [
-    [1, 2, 3],[4, 5, 6],[7, 8, 9],
-    [1, 4, 7],[2, 5, 8],[3, 6, 9],
-    [1, 5, 9],[3, 5, 7]
+    [0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],
+    [1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]
 ];
 
-function checkWin() {
-    for(let i of winningCombinations) {
-        console.log(i)
+
+function checkWinner() {
+    for(let arr of winningCombinations) {
+        let plOne = arr.every(item => playerOne.includes(item))
+        let plTwo = arr.every(item => playerTwo.includes(item))
+        if(plOne) {
+            alert('player one won')
+        } else if (plTwo) {
+            alert('player two won')
+        } 
     }
+    console.log(playerOne)
+    console.log(playerTwo)
 }
